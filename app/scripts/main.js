273a2cd9e73adc8420dea05c7cb9133bdc7cb233
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-01-31 11:27:48 +0000

    home page for app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,18 @@
-jQuery(function ($) {
-
-  /* Homepage background video. */
-  var home_image = $('#home-image');
-  if(home_image.length) {
-    // if movile device.
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      // Don't autoplay the backgorund video.
-    } else {
-      home_image.addClass('video');
-      home_image.tubular({videoId: 'yDc4zjAdp5A', start: 15});
-    }
-  }
-
-  /* Contact email link */
-  $('a.email-link').attr("href", "mailto:team@ideastarter.co")
-
-});
+// jQuery(function ($) {
+//
+//   /* Homepage background video. */
+//   var home_image = $('#home-image');
+//   if(home_image.length) {
+//     // if movile device.
+//     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+//       // Don't autoplay the backgorund video.
+//     } else {
+//       home_image.addClass('video');
+//       home_image.tubular({videoId: 'yDc4zjAdp5A', start: 15});
+//     }
+//   }
+//
+//   /* Contact email link */
+//   $('a.email-link').attr("href", "mailto:team@ideastarter.co")
+//
+// });
